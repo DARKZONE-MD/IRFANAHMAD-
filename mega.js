10f1a73e9e6f4b6351b5551da8bceef8ea3e137b
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-23 09:49:46 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'tatek95102@nab4.com',
-    password: 'malik&&46855',
+    email: 'darkzonemd@gmail.com',
+    password: 'darkzonemd@gmail.com',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
